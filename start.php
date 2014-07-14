c4ad1007a9d4ae46847be848116df6fd4de731f2
Sam <samjd1993@gmail.com>
2014-07-15 09:08:47 +1000

    Various fixes

--- start.php
+++ start.php
@@ -9,32 +9,36 @@
 		function validateForm() {
 			var minimumLength = 5;
 			var maxLength = 25;
+			var username = document.getElementById('username').value;
 			var passcode = document.getElementById("passcode").value;
-			var valid = false;
 
-			// Check it is long enough.
-			if (passcode.length < minimumLength) {
+			// Form validation
+			if (username == "" || passcode == "") { //Check all fields are filled
+				alert("Error, not all fields are filled out.");
+				return false;
+			} else if (passcode.length < minimumLength) { // Check password is long enough.
 				alert("Error, passcode must be longer than " + minimumLength + " characters.");
 				return false;
-			} else if (passcode.length > 25){
+			} else if (passcode.length > 25){ // Check password is short enough.
 				alert("Error, passcode must be " + maxLength + " characters or less.")
-			}
-			else {
+				return false;
+			} else {
 				return true;
 			}
-
 		}
 	</script>
 </head>
 <body>
 	<div class="container">
-		<div class="slab small">
+		<div class="slab medium">
 			<?php if(isset($_GET['error']) && $_GET['error'] === 'passcode') : ?>
-				<p class='error'>Incorrect password</p>
+				<p class='error'>Incorrect username or password</p>
 			<?php endif ?>
 			<h1>Login</h1>
 			<form name='form' action='login.php' onsubmit="return(validateForm())" method='post'>
-				Passcode:<br><br>
+				Username:<br>
+				<input type='username' name='username' id='username'><br><br>
+				Passcode:<br>
 				<input type='password' name='passcode' id='passcode'><br><br>
 				<input type='submit'>
 			</form>
